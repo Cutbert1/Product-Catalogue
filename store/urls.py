e236ed95ef8a925d072387ef5ad632a4593eb529
Cutbert1 <cutbertogbozor@yahoo.co.uk>
2025-03-02 12:54:25 +0000

    Add product list and details html

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", views.ProductListView.as_view(), name="home"),
+    path("", views.product_list.as_view(), name="home"),
+    path('<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 # from django.http import HttpResponse
 from .models import Product
@@ -8,14 +8,13 @@ from .models import Product
 
 
 # def my_store(request):
+# return HttpResponse('Hello, Store')
 
-   # return HttpResponse('Hello, Store')
-
-class ProductListView(generic.ListView):
+class product_list(generic.ListView):
     """View to list of published products with pagination."""
 
     template_name = "store/index.html"
-    paginate_by = 9
+    paginate_by = 12
     queryset = None
 
     def get_queryset(self):
@@ -23,3 +22,27 @@ class ProductListView(generic.ListView):
         if self.queryset is None:
             self.queryset = Product.objects.filter(status=1)
         return self.queryset
+
+
+def product_detail(request, slug):
+    queryset = Product.objects.filter(status=1)
+    product = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        'store/product_detail.html',
+        {"product": product},
+    )
+
+
+# from django.shortcuts import render
+# from .models import Product
+# def product_list(request):
+    # products = Product.objects.all()
+    # return render(request, 'myapp/index.html', {'products': products})
+# def product_detail(request, pk):
+    # product = Product.objects.get(pk=pk)
+    # return render(request, 'myapp/index2.html', {'product': product})
+# from django.http import HttpResponse
+# def home(request):
+    # return HttpResponse('Hello, World!')
